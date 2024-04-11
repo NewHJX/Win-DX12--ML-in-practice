d4533ca282220aab50e7d444dda18741cf9d7cb6
junxionghe <junxionghe@tencent.com>
2024-04-11 09:49:55 +0800

    [add]add a sample: obj reg

---
+++ ML/YatraML/pch.h
@@ -0,0 +1,10 @@
+#include <winrt/Windows.AI.MachineLearning.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Graphics.Imaging.h>
+#include <winrt/Windows.Media.h>
+#include <winrt/Windows.Storage.h>
+
+#include <string>
+#include <fstream>
+
+#include <Windows.h>
